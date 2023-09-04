358a61f571972acce81742def3ec30a11a2d0db5
PWT <krakozyabra.li@gmail.com>
2023-09-04 09:21:58 +0000

    Dima changes

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,16 +1,22 @@
 // get the client
 import mysql from 'mysql2';
-import { DB_USER, DB_PASSWORD } from '$env/static/private';
+import { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, SOCKET_PATH, PROD, DB_NAME } from '$env/static/private';
 
 const config = {
-	host: '127.0.0.1',
-	port: 8889,
+	host: DB_HOST,
+	port: DB_PORT,
 	user: DB_USER,
 	password: DB_PASSWORD,
-	database: 'personal_word_trainer',
+	database: DB_NAME,
+	// socketPath: PROD ? SOCKET_PATH : undefined
 };
 
-export const pool = mysql.createPool(config).promise();
+
+const createdPool = mysql.createPool(config)
+createdPool.on('connection', () => {
+	console.log('DataBase connected')
+})
+export const pool = createdPool.promise();
 
 // @ts-ignore
 export function runQuery(query) {
