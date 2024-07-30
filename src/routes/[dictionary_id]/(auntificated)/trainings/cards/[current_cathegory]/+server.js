4f26436494004b0a255db54823f6973872127c2d
kot-pomenshe <krakozyabra.li@gmail.com>
2024-07-31 02:04:38 +0300

    статистика обучения

--- src/routes/[dictionary_id]/(auntificated)/trainings/cards/[current_cathegory]/+server.js
+++ src/routes/[dictionary_id]/(auntificated)/trainings/cards/[current_cathegory]/+server.js
@@ -1,5 +1,6 @@
 import { json } from '@sveltejs/kit';
 import trainings from '$lib/server/models/trainings';
+import statistics from '$lib/server/models/statistics';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({request, locals, cookies, params}) {
@@ -22,5 +23,10 @@ export async function POST({request, locals, cookies, params}) {
     }
     const dictionary_id = params.dictionary_id;
     trainings.set_statistics(locals.session, dictionary_id, date, result);
+
+    let training_type = 21;
+
+    statistics.set_research_stat({user_id:locals.session, last_training_time: date, training_type: training_type, trainings_amount: null, first_training_time: null, mistakes_amount: null});
+
     return json({ok:true});
 }
--- src/routes/[dictionary_id]/(auntificated)/trainings/just_watch/[current_cathegory]/+server.js
+++ src/routes/[dictionary_id]/(auntificated)/trainings/just_watch/[current_cathegory]/+server.js
@@ -12,5 +12,10 @@ export async function POST({request, locals, cookies, params}) {
     //trainings_amount = trainings_amount + 1;
     console.log(`SCORE 2: `, total_score, trainings_amount);
     statistics.update_score({user_id, dictionary_id, total_score, trainings_amount});
+
+    let date =  new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+    let training_type = 11;
+    statistics.set_research_stat({user_id:locals.session, last_training_time: date, training_type: training_type, trainings_amount: null, first_training_time: null, mistakes_amount: null});
     return json({ok:true});
 }
